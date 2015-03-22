d249efb2cd57a2825c6352798ac750a2e6b8f59d
Aesen Vismea <aesen@gameminers.com>
2015-03-21 20:31:13 -0400

    Oops

--- src/main/java/com/gameminers/farrago/proxy/ServerProxy.java
+++ src/main/java/com/gameminers/farrago/proxy/ServerProxy.java
@@ -1,15 +1,20 @@
 package com.gameminers.farrago.proxy;
 
+import java.io.File;
 import java.util.Random;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.world.World;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 
+import com.gameminers.farrago.FarragoMod;
 import com.gameminers.farrago.Masses;
 import com.gameminers.farrago.entity.EntityRifleProjectile;
 import com.gameminers.farrago.enums.RifleMode;
+import com.typesafe.config.ConfigException;
+import com.typesafe.config.ConfigFactory;
 
 import cpw.mods.fml.common.registry.GameData;
 
@@ -33,7 +38,17 @@ public class ServerProxy implements Proxy {
 		Masses.bake();
 	}
 	@Override public void init() {}
-	@Override public void preInit() {}
+	@Override
+	public void preInit() {
+		File configFile = new File(Minecraft.getMinecraft().mcDataDir, "config/farrago.conf");
+		FarragoMod.config = ConfigFactory.parseFile(configFile);
+		try {
+			FarragoMod.brand = FarragoMod.config.getString("modpack.brand");
+		} catch (ConfigException.Null ex) {
+			FarragoMod.brand = null;
+		}
+		FarragoMod.showBrand = FarragoMod.config.getBoolean("modpack.showBrand");
+	}
 	@Override public void spawnRifleParticle(RifleMode mode, EntityRifleProjectile proj) {}
 	@Override public void stopSounds() {}
 	@Override public void glowRandomDisplayTick(World world, int x, int y, int z,Random rand) {}
